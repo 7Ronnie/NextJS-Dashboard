768a0ec5e39488af17d5789faa320097526d1ff7
kunalchoudhary619 <kunalchoudhary619.kc@gmail.com>
2023-12-19 16:42:51 +0530

    Next Commit

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,10 +9,14 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
+
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  noStore();
+
 
   try {
     // Artificially delay a response for demo purposes.
@@ -33,6 +37,8 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore();
+
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -53,6 +59,8 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore();
+
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -89,10 +97,14 @@ export async function fetchCardData() {
 
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
+  
   query: string,
   currentPage: number,
+  
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+  noStore();
+
 
   try {
     const invoices = await sql<InvoicesTable>`
@@ -124,6 +136,8 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  noStore();
+
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -145,6 +159,8 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore();
+
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -170,6 +186,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+
   try {
     const data = await sql<CustomerField>`
       SELECT
